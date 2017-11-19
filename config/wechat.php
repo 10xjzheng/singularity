5ccdd4354f8badbb095edb08d4debace7a165889
10xjzheng <10xjzheng@gmail.com>
2017-11-19 18:50:48 +0800

    update

--- config/wechat.php
+++ config/wechat.php
@@ -16,9 +16,9 @@ return [
     /*
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
-    'app_id'  => env('WECHAT_APPID', 'zzzzzzzwx2692cbf44150aa46'),         // AppID
-    'secret'  => env('WECHAT_SECRET', 'xxxxxxf4cf30bc4054e39708afeffc723b386c'),     // AppSecret
-    'token'   => env('WECHAT_TOKEN', 'yyyyyysglpay'),          // Token
-    'aes_key' => env('WECHAT_AES_KEY', 'wwwwwwwwwyWzpfbDfOtGkxhlWQzG0cOc3w6zZrOtAy6Ux6k9YtxG'),                    // EncodingAESKey
+    'app_id'  => env('WECHAT_APPID', 'zzzzzzz'),         // AppID
+    'secret'  => env('WECHAT_SECRET', 'xxxxxx'),     // AppSecret
+    'token'   => env('WECHAT_TOKEN', 'yyyyyy'),          // Token
+    'aes_key' => env('WECHAT_AES_KEY', 'wwwwwwwww'),                    // EncodingAESKey
 
 ];
